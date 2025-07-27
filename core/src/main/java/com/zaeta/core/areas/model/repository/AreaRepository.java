5100077947d015bfe895d3423f486a4d6ef9f2c3
andy <andygomezmojica@gmail.com>
2025-07-27 14:16:08 -0600

    Add getAllAreas module
    -Add getAllAreas UC
    -Add findByAreaIsActive method
    -Add getAllAreas Controller

--- core/src/main/java/com/zaeta/core/areas/model/repository/AreaRepository.java
+++ core/src/main/java/com/zaeta/core/areas/model/repository/AreaRepository.java
@@ -3,7 +3,11 @@ package com.zaeta.core.areas.model.repository;
 import com.zaeta.core.areas.model.AreaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AreaRepository extends JpaRepository<AreaModel, Long> {
 
     Boolean existsByAreaName(String areaName);
+
+    List<AreaModel> findByAreaIsActive(Boolean isActive);
 }
