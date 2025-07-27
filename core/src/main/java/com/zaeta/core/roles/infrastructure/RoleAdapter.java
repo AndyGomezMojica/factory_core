93ea08583d00c4a03cb4b43bc09d64b23485b682
andy <andygomezmojica@gmail.com>
2025-07-27 11:52:01 -0600

    Add modified role module
    -Add modifiedRoleUC
    -Add modifiedInput Maper
    -Add modifiedRoleController
    -Add modifiedRoleInput

--- core/src/main/java/com/zaeta/core/roles/infrastructure/RoleAdapter.java
+++ core/src/main/java/com/zaeta/core/roles/infrastructure/RoleAdapter.java
@@ -1,7 +1,9 @@
 package com.zaeta.core.roles.infrastructure;
 
 import com.zaeta.core.roles.model.RoleModel;
+import com.zaeta.core.roles.model.repository.RoleRepository;
 import com.zaeta.core.roles.web.responses.input.CreateRoleInput;
+import com.zaeta.core.roles.web.responses.input.ModifiedRoleInput;
 import com.zaeta.core.utils.GenericValidation.GenericValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,9 @@ public class RoleAdapter {
     @Autowired
     GenericValidation genericValidation;
 
+    @Autowired
+    RoleRepository repository;
+
     public RoleModel inputToModel(CreateRoleInput input){
 
         genericValidation.validateNotNull(input, "El input no puede estar vacio");
@@ -24,6 +29,17 @@ public class RoleAdapter {
                 .roleCreatedAt(new Date())
                 .roleIsActive(true)
                 .build();
+    }
+
+    public RoleModel modifiedInputToModel(RoleModel existentRole ,ModifiedRoleInput input){
+
+        existentRole.setRoleName(input.getRoleName().toUpperCase());
+        existentRole.setRoleDescription(input.getRoleDescription());
+        existentRole.setRoleIsActive(input.getRoleIsActive());
+        existentRole.setRoleModifiedAt(new Date());
+
+        return existentRole;
+
     }
 
 }
